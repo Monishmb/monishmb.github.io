34a6c434a542e0a651d07b1e0f3bdbae062c51ce
Harshit Raheja <quantumbytestudios@gmail.com>
2023-10-15 01:44:02 +0530

    Update

--- sendemail.php
+++ sendemail.php
@@ -1,47 +1,47 @@
 <?php
 
-$EmailFrom="jodhwanimadhur@gmail.com";
-$EmailTo="contact@mb-creatives.com";
-$Subject="Email from the Contact Form";
-$Name=Trim(stripslashes($_POST['name']));
-$Email=Trim(stripslashes($_POST['email']));
-$Subject=Trim(stripslashes($_POST['subject']));
-$Message=Trim(stripslashes($_POST['message']));
+$EmailFrom = "jodhwanimadhur@gmail.com";
+$EmailTo = "contact@mb-creatives.com";
+$Subject = "Email from the Contact Form";
+$Name = Trim(stripslashes($_POST['name']));
+$Email = Trim(stripslashes($_POST['email']));
+$Subject = Trim(stripslashes($_POST['subject']));
+$Message = Trim(stripslashes($_POST['message']));
 
 // simple way to validate the form
-$ValidationOk=true;
-if ($Name == "") $ValidationOk=false;
-	if (!$ValidationOk) {
-		echo "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
-		exit;
-	}
-		
-	// preparing the body of the email 
-	$Body="";
-	$Body.="Name: ";
-	$Body.=$Name;
-	$Body.="\n";
+$ValidationOk = true;
+if ($Name == "")
+	$ValidationOk = false;
+if (!$ValidationOk) {
+	echo "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+	exit;
+}
 
-	$Body.="Email: ";
-	$Body.=$Email;
-	$Body.="\n";
+// preparing the body of the email 
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
 
-	$Body.="Message: ";
-	$Body.=$Message;
-	$Body.="\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
 
-	//sending the email now
-	$success=mail($EmailTo, $Subject, $Body,"From: <$EmailFrom>");
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
 
-	//redirect after mail send 
-	if ($success) {
- 
-       print "<meta http-equiv=\"refresh\" content=\"0;URL=send.html\">";
+//sending the email now
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 
-	}
-	else {
+//redirect after mail send 
+if ($success) {
 
-		print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=send.html\">";
 
-	}
+} else {
+
+	print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+
+}
 ?>
